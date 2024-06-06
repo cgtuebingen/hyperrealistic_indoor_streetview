b19a20dd0f290de53f2b01d8c631b8c681290024
Pascal <callonz@protonmail.com>
2024-06-06 19:47:43 +0200

    implement basic fetching of files in canvas

--- app/components/CanvasLayer.tsx
+++ app/components/CanvasLayer.tsx
@@ -6,10 +6,23 @@ import {
     StatsGl
 } from '@react-three/drei';
 import { FirstPersonControls } from './FirstPersonControls.tsx'
+import { useFetcher, useLoaderData } from "@remix-run/react";
 
 const CanvasLayer = () => {
 
   const [splatExists, setSplatExists] = useState(false);
+  const [splatUrls, setSplatUrls] = useState<string[]>([]);
+
+  // fetch list of files from backend & filter them
+  useEffect(() => {
+    fetch('/files')
+      .then(response => response.json())
+      .then(data => setSplatUrls(data.filter(file => file.endsWith('.splat'))))
+      .catch(error => console.error('Error fetching files:', error));
+  }, []);
+
+  // temp debug: just shows the files
+  console.log(splatUrls);
 
   useEffect(() => {
     const checkFileExists = async () => {
