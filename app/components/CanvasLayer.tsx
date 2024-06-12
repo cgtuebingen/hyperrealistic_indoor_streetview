50325d6c520db3fda26f5c1eb414936cba85246e
NorbiPDF <noba023@gmail.com>
2024-06-13 01:49:05 +0200

    Added basic menu to select .splat files

--- app/components/CanvasLayer.tsx
+++ app/components/CanvasLayer.tsx
@@ -12,6 +12,8 @@ const CanvasLayer = () => {
 
   const [splatExists, setSplatExists] = useState(false);
   const [splatUrls, setSplatUrls] = useState<string[]>([]);
+  const [selectedSplat, setSelectedSplat] = useState<string>('');
+  const [isPointerLocked, setIsPointerLocked] = useState(true);
 
   // fetch list of files from backend & filter them
   useEffect(() => {
@@ -41,20 +43,35 @@ const CanvasLayer = () => {
     checkFileExists();
   }, []);
 
+  const handleSplatChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedSplat(event.target.value);
+  };
+
+  const handleOverlayEnter = () => setIsPointerLocked(false);
+  const handleOverlayLeave = () => setIsPointerLocked(true);
+
   return (
     <div className="absolute w-full h-full">
+      <div onMouseEnter={handleOverlayEnter} onMouseLeave={handleOverlayLeave}>
+        <select
+          value={selectedSplat}
+          onChange={handleSplatChange}
+          style={{margin:'0px 300px', position: 'absolute', zIndex: 10}}>
+            <option value="">Select Splat</option>
+            {splatUrls.map((url, index) => (
+              <option key={index} value={url}>
+                {url}
+              </option>
+            ))}
+          </select>
+      </div>
       <Canvas>
         <StatsGl />
         <ambientLight />
         <pointLight position={[0, 0, 0]} />
         <FirstPersonControls />
-        <PointerLockControls />
-        {splatExists &&
-        <Splat  position={[0, 2, 1]} src="splat.splat" /> }
-        {!splatExists &&
-        <Splat
-        position={[0, 2, 1]}
-        src="https://huggingface.co/cakewalk/splat-data/resolve/main/nike.splat" />}
+        {isPointerLocked && <PointerLockControls />}
+        <Splat position={[0, 2, 1]} src={selectedSplat || "https://huggingface.co/cakewalk/splat-data/resolve/main/nike.splat"} />
       </Canvas>
     </div>
   );
